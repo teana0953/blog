218e846be98f8700849c964509478a0578672412
teana0953 <teana0953@gmail.com>
2020-11-30 16:12:08 +0800

    Add post-create

---
+++ client/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PostCreate from './post-create';
+
+export default () => {
+    return (
+        <div className="container">
+            <h1>Create Post</h1>
+            <PostCreate />
+        </div>
+    );
+};
